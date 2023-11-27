8cf388c5c3df94a4d27081ca93bf2208331cceb5
Khoi Nguyen <vngu9798@uni.sydney.edu.au>
2023-11-27 22:48:28 +1100

    move test block to main

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import sklearn
+import time
+import os
+from semantic_analyzer import LSA
+
+#if you get an error in regards to stopwords resources not found uncomment the sections below
+# import nltk
+# nltk.download('stopwords')
+curr_test = 'test1.html'
+curr_path = os.path.dirname(os.path.realpath(__file__))
+target_path = f'{curr_path}/tests/{curr_test}'
+
+with open(file = target_path, mode = 'r', encoding = 'utf-8') as file:
+    html_content = file.read()
+
+start_time = time.time()
+LSA(html_content, dbug = False)
+end_time = time.time()
+
+print(f'Total runtime: {end_time - start_time}')
--- semantic_analyzer.py
+++ semantic_analyzer.py
@@ -73,19 +73,3 @@ def LSA(html: str, dbug: bool = False) -> None:
         print('Sparse Matrix Shape:', X.shape)
         print('LSA Components:', lsa.components_[0])
 
-if __name__ == '__main__':
-    #if you get an error in regards to stopwords resources not found uncomment the sections below
-    # import nltk
-    # nltk.download('stopwords')
-    curr_test = 'test1.html'
-    curr_path = os.path.dirname(os.path.realpath(__file__))
-    target_path = f'{curr_path}/tests/{curr_test}'
-
-    with open(file = target_path, mode = 'r', encoding = 'utf-8') as file:
-        html_content = file.read()
-
-    start_time = time.time()
-    LSA(html_content, dbug = False)
-    end_time = time.time()
-
-    print(f'Total runtime: {end_time - start_time}')
